09e3e9c88c29d9b134ca955f486f2a6eceb24d2a
Davi Mordonho <85120918+Davi-Lv@users.noreply.github.com>
2022-06-29 22:02:32 -0300

    Add files via upload

---
+++ ImprimirTeclado.java
@@ -0,0 +1,52 @@
+package postados;
+import java.util.Scanner;
+
+public class ImprimirTeclado {
+	public ImprimirTeclado() {
+
+	}
+
+	// pode adiconar qual quer variavel para imprimir
+	String fistNome;
+	int cpf;
+
+	// construtor
+	public ImprimirTeclado(String fistNome, int cpf) {
+		this.fistNome = fistNome;
+		this.cpf = cpf;
+	}
+
+	// metodo main
+	public static void main(String[] args) {
+
+		// imprimir texto para o usuario
+		System.out.println("Digite seu nome");
+
+		// ler oque o usuario digitou
+		Scanner tecla = new Scanner(System.in);
+
+		// criando um objeto
+		ImprimirTeclado t = new ImprimirTeclado();
+
+		// dando um valor para a variavel nome de acordo com oque o usuario digitar
+		t.fistNome = tecla.next();
+
+		System.out.println("-------------------");
+
+		// repetindo tudo para cpf
+		System.out.println("Digite seu cpf");
+		t.cpf = tecla.nextInt();
+
+		// chamar o void imprimir
+		t.imprimir();
+		
+	}
+
+	// void para imprimir os dados
+	public void imprimir() {
+		System.out.println("-------------------");
+		System.out.println("Nome: " + fistNome);
+		System.out.println("cpf: " + cpf);
+		System.out.println("-------------------");
+	}
+}
