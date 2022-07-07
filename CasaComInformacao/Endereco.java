23cdfac82b2850c605dbbfc636e89c8e897a9d72
Davi Mordonho <85120918+Davi-Lv@users.noreply.github.com>
2022-07-07 15:26:34 -0300

    Add files via upload

---
+++ CasaComInformacao/Endereco.java
@@ -0,0 +1,19 @@
+package CasaComInformacao;
+
+public class Endereco {
+	String rua;
+	int numero;
+	String bairro;
+	String cidade;
+
+	public Endereco() {
+
+	}
+	
+	public Endereco(String rua, int numero, String bairro, String cidade) {
+		this.rua = rua;
+		this.numero = numero;
+		this.bairro = bairro;
+		this.cidade = cidade;
+	}
+}
