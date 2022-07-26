442faa97f7811dcd04e132532c761c12dd9ff322
Davi Mordonho <85120918+Davi-Lv@users.noreply.github.com>
2022-07-26 11:35:50 -0300

    Add files via upload

---
+++ CasaComInformacao/Porta.java
@@ -0,0 +1,15 @@
+package CasaComInformacao;
+
+public class Porta {
+	double altura;
+	String cor;
+
+	public Porta() {
+
+	}
+
+	public Porta(double altura, String cor) {
+		this.altura = altura;
+		this.cor = cor;
+	}
+}
