b21a2b6bba0528a4ff7aa376ab16902340bc062e
Davi Mordonho <85120918+Davi-Lv@users.noreply.github.com>
2022-07-29 10:27:55 -0300

    Add files via upload

---
+++ CasaComInformacao/Proprietario.java
@@ -0,0 +1,15 @@
+package CasaComInformacao;
+
+public class Proprietario {
+	String nome;
+	String telefone;
+
+	public Proprietario() {
+
+	}
+
+	public Proprietario(String nome, String telefone) {
+		this.nome = nome;
+		this.telefone = telefone;
+	}
+}
